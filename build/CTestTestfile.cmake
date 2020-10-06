facd2c5581ab229d3504ccc6f648711dfda9d43e
Michiya Imachi <michiya.imachi@gmail.com>
2020-10-06 20:33:38 +0900

    update

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/imachi/work_space/git_workspace/std_string_wapper
-# Build directory: /home/imachi/work_space/git_workspace/std_string_wapper/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("test")
--- build/test/string_editor/googletest-prefix/src/googletest-build/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/imachi/work_space/git_workspace/std_string_wapper/build/test/string_editor/googletest-prefix/src/googletest/googlemock
-# Build directory: /home/imachi/work_space/git_workspace/std_string_wapper/build/test/string_editor/googletest-prefix/src/googletest-build/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
