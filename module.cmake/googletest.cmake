1c15458bb214c1ac6234c550b38360385dd0a803
Michiya Imachi <michiya.imachi@gmail.com>
2020-10-13 20:25:27 +0900

    update CMakeLists.txt and its modules

---
+++ module.cmake/googletest.cmake
@@ -0,0 +1,77 @@
+#<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+macro(SET_GOOGLETEST_VARIABLES)
+    if(NOT DEFINED GOOGLETEST_INSTALL_DESTINATION)
+        set(GOOGLETEST_INSTALL_DESTINATION ${CMAKE_BINARY_DIR}/googletest)
+        set(GOOGLETEST_ROOT ${GOOGLETEST_INSTALL_DESTINATION})
+        set(GOOGLETEST_INCLUDE_DIR ${GOOGLETEST_ROOT}/include)
+        set(GOOGLETEST_LIBRARY_DIR ${GOOGLETEST_ROOT}/lib)
+        set(GOOGLETEST_LIB gtest;gtest_main;pthread)
+    endif()
+endmacro()
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+#<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+macro(fetch_googletesst)
+    if(NOT TARGET EXTERNAL_GOOGLETEST_PROJECT)
+        SET_GOOGLETEST_VARIABLES()
+        include(ExternalProject)
+        ExternalProject_Add(
+            EXTERNAL_GOOGLETEST_PROJECT
+            GIT_REPOSITORY https://github.com/google/googletest
+            CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${GOOGLETEST_INSTALL_DESTINATION}
+        )
+    endif()
+endmacro()
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+#<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+macro(LOAD_DEPENDENCIES_GOOGLETEST_MODULE MACRO_ARG)
+    if(NOT TARGET EXTERNAL_GOOGLETEST_PROJECT)
+        fetch_googletesst()
+    endif()
+    SET_GOOGLETEST_VARIABLES()
+    add_dependencies(${MACRO_ARG} EXTERNAL_GOOGLETEST_PROJECT)
+endmacro()
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+#<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+macro(LOAD_LINK_DIRS_GOOGLETEST_MODULE MACRO_ARG)
+    if(NOT TARGET EXTERNAL_GOOGLETEST_PROJECT)
+        fetch_googletesst()
+    endif()
+    SET_GOOGLETEST_VARIABLES()
+    target_link_directories(
+        ${MACRO_ARG}
+        PRIVATE ${GOOGLETEST_LIBRARY_DIR}
+    )
+endmacro()
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+#<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+macro(LOAD_LINK_GOOGLETEST_MODULE MACRO_ARG)
+    if(NOT TARGET EXTERNAL_GOOGLETEST_PROJECT)
+        fetch_googletesst()
+    endif()
+    SET_GOOGLETEST_VARIABLES()
+    target_link_libraries(
+        ${MACRO_ARG}
+        PRIVATE ${GOOGLETEST_LIB}
+        )
+endmacro()
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+#<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+macro(LOAD_INCLUDE_GOOGLETEST_MODULE MACRO_ARG)
+    if(NOT TARGET EXTERNAL_GOOGLETEST_PROJECT)
+        fetch_googletesst()
+    endif()
+    SET_GOOGLETEST_VARIABLES()
+    target_include_directories(
+        ${MACRO_ARG}
+        PRIVATE ${GOOGLETEST_INCLUDE_DIR}
+        )
+endmacro()
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+#<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+macro(LOAD_SETTING_GOOGLETEST_MODULE MACRO_ARG)
+    LOAD_DEPENDENCIES_GOOGLETEST_MODULE(${MACRO_ARG})
+    LOAD_LINK_DIRS_GOOGLETEST_MODULE(${MACRO_ARG})
+    LOAD_LINK_GOOGLETEST_MODULE(${MACRO_ARG})
+    LOAD_INCLUDE_GOOGLETEST_MODULE(${MACRO_ARG})
+endmacro()
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
--- module.cmake/utility.cmake
+++ module.cmake/utility.cmake
@@ -1,2 +1,43 @@
-set(UTILITY_LIB "utility")
-set(UTILITY_INCLUDE_DIR "${CMAKE_CURRENT_LIST_DIR}/../src/")
+#<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+set_property(GLOBAL PROPERTY PATH_UTILITY_MODULE "${CMAKE_CURRENT_LIST_DIR}")
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+#<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+macro(SET_UTILITY_VARIABLES)
+    get_property(my_module_path GLOBAL PROPERTY PATH_UTILITY_MODULE)
+    if(NOT DEFINED UTILITY_LIB_NAME)
+        set(UTILITY_LIB_NAME "michiya_utility")
+        set(UTILITY_INCLUDE_DIR "${my_module_path}/../src/")
+    endif()
+endmacro()
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+#<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+macro(LOAD_DEPENDENCIES_UTILITY_MODULE MACRO_ARG)
+    SET_UTILITY_VARIABLES()
+    add_dependencies(${MACRO_ARG} ${UTILITY_LIB_NAME})
+endmacro()
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+#<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+macro(LOAD_LINKS_UTILITY_MODULE MACRO_ARG)
+    SET_UTILITY_VARIABLES()
+    target_link_libraries(
+        ${MACRO_ARG}
+        PRIVATE ${UTILITY_LIB_NAME}
+        )
+endmacro()
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+#<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+macro(LOAD_INCLUDES_UTILITY_MODULE MACRO_ARG)
+    SET_UTILITY_VARIABLES()
+    target_include_directories(
+        ${MACRO_ARG}
+        PRIVATE ${UTILITY_INCLUDE_DIR}
+    )
+endmacro()
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+#<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+macro(LOAD_SETTING_UTILITY_MODULE MACRO_ARG)
+    LOAD_DEPENDENCIES_UTILITY_MODULE(${MACRO_ARG})
+    LOAD_LINKS_UTILITY_MODULE(${MACRO_ARG})
+    LOAD_INCLUDES_UTILITY_MODULE(${MACRO_ARG})
+endmacro()
+#<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
